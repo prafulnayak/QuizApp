3ab76a45ee6c56a86c76a65a4dafd2474e87d434
Praful Nayak <prafulnayak1988@gmail.com>
2018-07-03 15:01:19 +0530

    Question Answer Validation with color: Green for Right & Red for Wrong Answer.

--- app/src/main/java/org/sairaa/quizapp/MainActivity.java
+++ app/src/main/java/org/sairaa/quizapp/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText q4Answer, q5Answer;
     private Button submit;
     private RadioButton radioButton;
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,37 +49,166 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void checkQuestion5() {
-        
+        String answer = q5Answer.getText().toString().trim().toLowerCase();
+        if(!answer.equals("")){
+            if(getString(R.string.q5Answer).trim().toLowerCase().equals(answer)){
+                q5Answer.setTextColor(getResources().getColor(R.color.green));
+                score++;
+            }else {
+                q5Answer.setTextColor(getResources().getColor(R.color.red));
+            }
+        }
+
     }
 
     private void checkQuestion4() {
+        String answer = q4Answer.getText().toString().trim().toLowerCase();
+        if(!answer.equals("")){
+            if(getString(R.string.q4Answer).trim().toLowerCase().equals(answer)){
+                q4Answer.setTextColor(getResources().getColor(R.color.green));
+                score++;
+            }else {
+                q4Answer.setTextColor(getResources().getColor(R.color.red));
+            }
+        }
+
 
     }
 
     private void checkQuestion3() {
+        int selectedID2 = radioGroup3.getCheckedRadioButtonId();
+        int rightAnswer = Integer.parseInt(getString(R.string.q3Answer));
+        RadioButton r1,r2,r3,r4;
+        r1 = findViewById(R.id.radioGroup3Option1);
+        r2 = findViewById(R.id.radioGroup3Option2);
+        r3 = findViewById(R.id.radioGroup3Option3);
+        r4 = findViewById(R.id.radioGroup3Option4);
+        //set color for right answer
+        switch (rightAnswer){
+            case 1:
+                r1.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case 2:
+                r2.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case 3:
+                r3.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case 4:
+                r4.setTextColor(getResources().getColor(R.color.green));
+                break;
+            default:
+
+        }
+        // find the radiobutton by returned id
+        Toast.makeText(MainActivity.this,""+selectedID2,Toast.LENGTH_SHORT).show();
+        if(selectedID2 != -1){
+            radioButton = findViewById(selectedID2);
+            switch (selectedID2){
+                case R.id.radioGroup3Option1:
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option1))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 1",Toast.LENGTH_SHORT).show();
+
+                    break;
+                case R.id.radioGroup3Option2:
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option2))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 2",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.radioGroup3Option3:
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option3))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 3",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.radioGroup3Option4:
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option4))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 4",Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+
+            }
+        }
+
 
     }
 
     private void checkQuestion2() {
-        int answer2 = 0;
+//        int answer2 = 0;
         int selectedID2 = radioGroup2.getCheckedRadioButtonId();
+        int rightAnswer = Integer.parseInt(getString(R.string.q2Answer));
+        RadioButton r1,r2,r3,r4;
+        r1 = findViewById(R.id.radioGroup2Option1);
+        r2 = findViewById(R.id.radioGroup2Option2);
+        r3 = findViewById(R.id.radioGroup2Option3);
+        r4 = findViewById(R.id.radioGroup2Option4);
+        //set color for right answer
+        switch (rightAnswer){
+            case 1:
+                r1.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case 2:
+                r2.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case 3:
+                r3.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case 4:
+                r4.setTextColor(getResources().getColor(R.color.green));
+                break;
+            default:
+
+        }
         // find the radiobutton by returned id
         Toast.makeText(MainActivity.this,""+selectedID2,Toast.LENGTH_SHORT).show();
         if(selectedID2 != -1){
             radioButton = findViewById(selectedID2);
             switch (selectedID2){
                 case R.id.radioGroup2Option1:
-                    Toast.makeText(MainActivity.this,"opt 1",Toast.LENGTH_SHORT).show();
-                    radioButton.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option1))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 1",Toast.LENGTH_SHORT).show();
+
                     break;
                 case R.id.radioGroup2Option2:
-                    Toast.makeText(MainActivity.this,"opt 2",Toast.LENGTH_SHORT).show();
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option2))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 2",Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.radioGroup2Option3:
-                    Toast.makeText(MainActivity.this,"opt 3",Toast.LENGTH_SHORT).show();
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option3))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 3",Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.radioGroup2Option4:
-                    Toast.makeText(MainActivity.this,"opt 4",Toast.LENGTH_SHORT).show();
+                    if(rightAnswer == Integer.parseInt(getString(R.string.option4))){
+                        score++;
+                        radioButton.setTextColor(getResources().getColor(R.color.green));
+                    }else
+                        radioButton.setTextColor(getResources().getColor(R.color.red));
+//                    Toast.makeText(MainActivity.this,"opt 4",Toast.LENGTH_SHORT).show();
                     break;
                 default:
 
@@ -88,9 +218,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void checkQuestion1() {
-        Boolean option1 = q1Option1.isChecked();
-        Boolean option2 = q1Option2.isChecked();
-        Boolean option3 = q1Option3.isChecked();
-        Boolean option4 = q1Option4.isChecked();
+        int rightAnswer = Integer.parseInt(getString(R.string.q1Answer));
+
+        switch (rightAnswer){
+            case 1:
+                q1Option1.setTextColor(getResources().getColor(R.color.green));
+                if(q1Option1.isChecked() && !q1Option2.isChecked() && !q1Option3.isChecked() && !q1Option4.isChecked()){
+                    score++;
+//                    q1Option1.setTextColor(getResources().getColor(R.color.green));
+                }
+                if(q1Option2.isChecked()){
+                    q1Option2.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option3.isChecked()){
+                    q1Option3.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option4.isChecked()){
+                    q1Option4.setTextColor(getResources().getColor(R.color.red));
+                }
+
+                break;
+            case 2:
+                q1Option2.setTextColor(getResources().getColor(R.color.green));
+                if(!q1Option1.isChecked() && q1Option2.isChecked() && !q1Option3.isChecked() && !q1Option4.isChecked()){
+                    score++;
+
+                }
+                if(q1Option1.isChecked()){
+                    q1Option1.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option3.isChecked()){
+                    q1Option3.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option4.isChecked()){
+                    q1Option4.setTextColor(getResources().getColor(R.color.red));
+                }
+
+                break;
+            case 3:
+                q1Option3.setTextColor(getResources().getColor(R.color.green));
+                if(!q1Option1.isChecked() && !q1Option2.isChecked() && q1Option3.isChecked() && !q1Option4.isChecked()){
+                    score++;
+
+                }
+                if(q1Option1.isChecked()){
+                    q1Option1.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option2.isChecked()){
+                    q1Option2.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option4.isChecked()){
+                    q1Option4.setTextColor(getResources().getColor(R.color.red));
+                }
+                break;
+            case 4:
+                q1Option4.setTextColor(getResources().getColor(R.color.green));
+                if(!q1Option1.isChecked() && !q1Option2.isChecked() && !q1Option3.isChecked() && q1Option4.isChecked()){
+                    score++;
+
+                }
+                if(q1Option1.isChecked()){
+                    q1Option1.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option2.isChecked()){
+                    q1Option2.setTextColor(getResources().getColor(R.color.red));
+                }
+                if(q1Option3.isChecked()){
+                    q1Option3.setTextColor(getResources().getColor(R.color.red));
+                }
+                break;
+            default:
+
+        }
+
     }
 }
